f9eafaf7fe56b6a338f593fcda91a354bae5146b
Daniel TIllero <danieltillero67@gmail.com>
2020-10-06 16:14:32 -0400

    implement new-pic button

--- public/events.js
+++ public/events.js
@@ -1,12 +1,22 @@
-window.addEventListener("DOMContentLoaded", async () => {
-  try {
-    let res = await fetch("/kitten/image");
-    if (!res.ok) throw res;
-    let data = await res.json();
-    let img = document.querySelector(".cat-pic");
-    img.src = data.src;
-  } catch (error) {
-    let data = await error.json();
-    alert(data.message);
-  }
-});
+const getCatPic = async () => {
+    try {
+        let img = document.querySelector(".cat-pic");
+        document.querySelector(".loader").innerHTML = "<p>Loading..</p>";
+        img.src = "";
+
+        let res = await fetch("/kitten/image");
+        if (!res.ok) throw res;
+        let data = await res.json();
+
+        img.src = data.src;
+        document.querySelector(".loader").innerHTML = "";
+    } catch (error) {
+        let data = await error.json();
+        alert(data.message);
+    }
+};
+
+let button = document.querySelector("#new-pic");
+
+window.addEventListener("DOMContentLoaded", getCatPic);
+button.addEventListener("click", getCatPic);
